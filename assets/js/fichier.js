0c8630e893104836261b155801d41c2d28d76732
Estelle111 <Vous@exemple.com>
2019-02-27 17:00:36 +0100

    wip location

---
+++ assets/js/fichier.js
@@ -0,0 +1,19 @@
+var timerRunning = false;
+function runClock() 
+{
+  var today   = new Date();
+  var hours   = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  var timeValue = hours;
+ 
+  // Les deux prochaines conditions ne servent que pour l'affichage.
+  // Si le nombre de minutes est inférieur à 10, alors on ajoute un 0 devant...
+ 
+  timeValue += ((minutes < 10) ? ":0" : ":") + minutes;
+  timeValue += ((seconds < 10) ? ":0" : ":") + seconds;
+  document.getElementById("time").value = timeValue;
+  timerRunning = true;
+}
+ 
+var timerID = setInterval(runClock,1000);
